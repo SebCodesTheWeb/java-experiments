b8661d206fd2266fc8a13b15ea73322403286976
Johannes Larsson <johannes.larsson27@gmail.com>
2026-09-07 11:38:06 +0200

    hej

--- edaa01-workspace-vscode/lab1/src/game/Board.java
+++ edaa01-workspace-vscode/lab1/src/game/Board.java
@@ -1,20 +1,21 @@
 package game;
 
 public class Board {
+	public int numPins = 0;
 
 	public Board() {
 
 	}
 	
 	public void setUp(int numPins) {
-		
+		this.numPins = numPins;
 	}
 	
 	public void takePins(int nbr) {
-
+		this.numPins = this.numPins - nbr;
 	}
 
 	public int getNumPins() {
-		return -1;
+		return this.numPins;
 	}	
 }
